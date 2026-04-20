70f3e112566bf345d59e7746ba07fde5f79e275e
royshliang@outlook <liangsehhaw@outlook.my>
2026-04-20 20:46:28 +0800

    change pydantic model ref to def column name

--- utilities/structured_model.py
+++ utilities/structured_model.py
@@ -8,10 +8,10 @@ from typing import List, Optional
 class WorkoutExercise(BaseModel):
     id: int = Field(description="the id of the exercise from the provided data")
     name: str = Field(descipriont="the name of the exercise")
-    pre_sets: int = Field(description="no of sets to perfrom")
-    pre_reps: Optional[int] = Field(None, description="reptitions per set")
-    pre_weight: float = Field(0.0, description="suggested weight in kg")
-    pre_duration: int = Field(0, description="duration in minutes for cardio")
+    def_sets: int = Field(description="no of sets to perfrom")
+    def_reps: Optional[int] = Field(None, description="reptitions per set")
+    def_weight: float = Field(0.0, description="suggested weight in kg")
+    def_duration: int = Field(0, description="duration in minutes for cardio")
 
 class WorkoutPlan(BaseModel):
     exercises: List[WorkoutExercise]
